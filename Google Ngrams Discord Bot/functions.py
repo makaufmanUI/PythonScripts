1df3512daa62f524581589edaef52942b0e68a77
Matt Kaufman <98175105+makaufmanUI@users.noreply.github.com>
2022-07-28 16:19:50 -0500

    Add files via upload

---
+++ Google Ngrams Discord Bot/functions.py
@@ -0,0 +1,193 @@
+#####
+#
+#     Hanldes queries to google ngrams, returns a timeseries of the usage
+#       frequency in the google books corpus from 1800-2019 as a line chart
+#
+#####
+
+
+import json
+from pathlib import Path
+from matplotlib import pyplot
+from urllib.request import urlopen
+
+
+
+def raiseException(e, line, file=None):
+    """
+    Raises an exception (prints to console).
+    - `e`: exception
+    - `line`: line number
+    - `file`: file name (optional, default is None)
+    """
+    if file is None:
+        print(f"\n>>>  Exception at line {line}:")
+        for i in range(len(str(line))+24): print('-',end='')
+        print(f"\n{e}")
+    else:
+        file = file.split("\\")[-1]
+        print(f"\n>>>  Exception at line {line} in {file}:")
+        for i in range(len(str(line))+len(file)+28): print('-',end='')
+        print(f"\n{e}")
+
+
+
+
+#*********************************************************#
+#                                                         #
+#   Frequency data normalization function                 #
+#     Input:   raw timeseries data                        #
+#     Outputs: data normalized to minimum of 1,           #
+#              year in which the data is normalized to,   #
+#              year where the global maximum is reached   #
+#                                                         #
+#*********************************************************#
+def normalize(timeseries, start):
+    nozeros = [x for x in timeseries if x != 0]                 # remove zeros from the timeseries
+    smallest = min(nozeros)                                     # find minimum value in the timeseries
+    norm = [round(value/smallest,5) for value in timeseries]    # normalize the timeseries to the smallest value
+    biggest = max(norm)                                         # find the maximum value in the normalized timeseries
+    for i in range(len(norm)):
+        if norm[i] == 1:  smallest_year = i+start               # find the year in which the smallest value is reached
+        if norm[i] == biggest:  biggest_year = i+start          # find the year in which the biggest value is reached
+    return norm, smallest_year, biggest_year
+
+
+
+
+#*********************************************************#
+#                                                         #
+#   Main n-gram searching function                        #
+#     Input:   query string                               #
+#     Outputs: data normalized to minimum of 1,           #
+#              year in which the data is normalized to,   #
+#              year where the global maximum is reached,  #
+#              actual search term, start year, end year   #
+#                                                         #
+#*********************************************************#
+def get(query):
+    url, search, start, end = getURL(query)                            # get ngram url
+    with urlopen(url) as response:
+        datajson = json.loads(response.read())[0]                      # read the json data
+    try:  timeseries = datajson['timeseries']                          # extract the timeseries
+    except Exception as e:  raiseException(e,74,__file__)
+    norm, smallest_year, biggest_year = normalize(timeseries, start)   # normalize the timeseries
+    return norm, smallest_year, biggest_year, search, start, end
+
+
+
+
+#*********************************************************#
+#                                                         #
+#   URL getter function                                   #
+#     Input:   query string, start year,                  #
+#              end year, and smoothing amount             #
+#     Outputs: google ngram url string,                   #
+#              actual search term, start year, end year   #
+#                                                         #
+#*********************************************************#
+def getURL(query, start=1800, end=2019, smoothing=0, **kwargs):
+    if ',' in query:
+        split_str = query.split(',')                        # split the query string by commas
+
+        if split_str[0][0] != '.':  return                  # only accept queries beginning with '.'
+        else:  search = split_str[0][1:]                    # take everything from '.' to ',' for search string
+
+        if ' ' in search:
+            search = search.replace(' ', '+')               # spaces need to be '+'s in google ngram URL
+
+        for i in range(1, len(split_str)):
+            if 'start' in split_str[i]:
+                if '=' in split_str[i]:                                           # if the start year is specified with '='
+                    try:  start = int(split_str[i].split('=')[1])                   # get start year as an int
+                    except Exception as e:  raiseException(e,103,__file__)
+                elif 'at' in split_str[i]:                                        # if the start year is specified with 'at'
+                    try:  start = int(split_str[i].split('at')[1])                  # get start year as an int
+                    except Exception as e:  raiseException(e,106,__file__)
+                elif 'from' in split_str[i]:                                      # if the start year is specified with 'at'
+                    try:  start = int(split_str[i].split('from')[1])                # get start year as an int
+                    except Exception as e:  raiseException(e,109,__file__)
+
+            if 'end' in split_str[i]:
+                if '=' in split_str[i]:                                           # if the end year is specified with '='
+                    try:  end = int(split_str[i].split('=')[1])                     # get end year as an int
+                    except Exception as e:  raiseException(e,114,__file__)
+                elif 'at' in split_str[i]:                                        # if the end year is specified with 'at'
+                    try:  end = int(split_str[i].split('at')[1])                    # get end year as an int
+                    except Exception as e:  raiseException(e,117,__file__)
+                elif 'from' in split_str[i]:                                      # if the end year is specified with 'at'
+                    try:  end = int(split_str[i].split('from')[1])                  # get end year as an int
+                    except Exception as e:  raiseException(e,120,__file__)
+
+            if 'smoothing' in split_str[i]:
+                try:  smoothing = int(split_str[i].split('=')[1])                 # get smoothing amount as an int
+                except Exception as e:  raiseException(e,124,__file__)
+    
+    else:
+        search = query[1:]                                  # search string is everything after '.' in query string
+        if ' ' in search:
+            search = search.replace(' ', '+')               # spaces need to be '+'s in google ngram URL
+    
+    if start > end:  start, end = end, start                # start year cannot be greater than end year
+    if end > 2019 or end < 1500:  end = 2019                # end year can only be between 1500 and 2019
+    if smoothing < 0:  smoothing = -smoothing               # smoothing amount cannot be a negative number
+    if start > 2019 or start < 1500:  start = 1800          # start year can only be between 1500 and 2019
+    
+    url = f"https://books.google.com/ngrams/json?content={search}&year_start={start}&year_end={end}&corpus=26&smoothing={smoothing}&case_insensitive=true"
+    return url, search, start, end
+
+
+
+
+#*********************************************************#
+#                                                         #
+#   Step-size calculator                                  #
+#     Inputs:  start year, end year                       #
+#     Output:  an integer step-size,                      #
+#              such that  7 < x-axis tick count < 13      #
+#                                                         #
+#*********************************************************#
+def stepsize(start, end):
+    if   end-start < 30:   return 3
+    elif end-start > 29    and end-start <=  59:  return 5
+    elif end-start > 59    and end-start <= 104:  return 10
+    elif end-start > 104   and end-start <= 139:  return 15
+    elif end-start > 139   and end-start <  175:  return 20
+    elif end-start >= 175  and end-start <  324:  return 25
+    elif end-start >= 324  and end-start <  389:  return 30
+    elif end-start >= 389  and end-start <  454:  return 35
+    elif end-start >= 454: return 40
+
+
+
+
+#*********************************************************#
+#                                                         #
+#   Plot generating function                              #
+#     Input:  search string (passed to get function)      #
+#     Output: png image file (saves locally for main)     #
+#                                                         #
+#*********************************************************#
+def plot(query):
+    timeseries, smallest_year, biggest_year, search, start, end = get(query)
+    print(f"\n\n[plot function]>>>  Searching for {search} from {start} to {end}", end='')
+    print(f" with smallest_year = {smallest_year}, and bigggest_year = {biggest_year}\n\n")
+
+    step = stepsize(start,end)
+    pyplot.style.use('ggplot')
+    pyplot.rcParams['figure.figsize'] = (17, 10)
+    pyplot.tight_layout()
+    pyplot.subplots_adjust(right=0.93, bottom=0.15)
+
+    pyplot.title(f'"{search.replace("+", " ")}"', font=Path('font/JetBrainsMonoExtraBold.ttf'), fontsize = 35, pad = 30, color='white')
+    pyplot.xlabel('year', font=Path('font/JetBrainsMonoMediumItalic.ttf'), fontsize=33, labelpad=41, color='white')
+    pyplot.ylabel('frequency', font=Path('font/JetBrainsMonoMediumItalic.ttf'), fontsize=30, labelpad=38, color='white')
+    pyplot.xticks(range(start, end, step), font=Path('font/InconsolataRegular.ttf'), fontsize=30, color='white')
+    pyplot.yticks(font=Path('font/InconsolataRegular.ttf'), fontsize=30, color='white')
+
+    x = [i+start for i in range(len(timeseries))]
+    pyplot.plot(x,timeseries)
+    pyplot.axvline(x=biggest_year, color='#BBBBBB', linestyle='--')
+    pyplot.axvline(x=smallest_year, color='#BBBBBB', linestyle='--')
+    pyplot.savefig('pic.png', format='png', facecolor='#36393f')
+    pyplot.close()
---
+++ Google Ngrams Discord Bot/main.py
@@ -0,0 +1,147 @@
+#####
+#
+#     Hanldes queries to google ngrams, returns a timeseries of the usage
+#       frequency in the google books corpus from 1800-2019 as a line chart
+#
+#####
+
+
+import time
+import discord
+from datetime import datetime as dt
+from functions import plot, raiseException
+
+TOKEN = 'xxxxx'
+
+client = discord.Client()
+
+
+
+@client.event
+async def on_ready():
+    print(f"\n\n\n>>> Logged in as {client.user.name}, with Client ID [{client.user.id}]")
+    print(f"\n>>> Date:  {dt.now().strftime('%m-%d-%Y')},  Time:  {dt.now().strftime('%H:%M:%S')}")
+    print("\n>>> Current servers:  ", end='')
+    for guild in client.guilds:
+        print(guild.name, end='   ')
+    print("\n\n\n\n>>> Listening...\n\n")
+    await client.change_presence(activity=discord.Game(name='Listening for Queries'))
+
+
+
+
+@client.event
+async def on_message(message):
+    # DM handler
+    if not message.guild and message.author != client.user:
+        await message.channel.send("u r gay")
+        return
+
+    # Don't listen to self
+    if message.author == client.user:
+        return
+    
+    # Start timer
+    start = time.perf_counter()
+
+
+
+    #****************************#
+    #                            #
+    #    Search query handler    #
+    #                            #
+    #****************************#
+    if message.content.startswith('.'):
+        query = message.content
+        print(f"\n>>>  {message.author.name} sent:  '{query}'", end='')
+        try:  plot(query)
+        except Exception as e:  raiseException(e,61,__file__)
+        await message.channel.send(file=discord.File('pic.png'))
+        print(f"\t--> Completed query in {round(time.perf_counter()-start,3)} seconds\n")
+    
+
+
+    #********************************************************#
+    #                                                        #
+    #   Search history deletion handler (specified amount)   #
+    #                                                        #
+    #********************************************************#
+    if message.content.startswith('!delete') and 'all' not in message.content:
+        requestor = message.author
+        if len(message.content) < 8:
+            to_delete = 2
+        else:
+            to_delete_str = message.content[8:]
+            try:  to_delete = int(to_delete_str)*2
+            except:  to_delete = 2
+        if to_delete == 2:
+            print(f"\n>>>  {requestor.name} requested deletion of 1 item ({to_delete} messages).")
+        else:
+            print(f"\n>>>  {requestor.name} requested deletion of {int(to_delete/2)} items ({to_delete} messages).")
+
+        if to_delete > 6:
+            await message.channel.send(f"{requestor.name}, you can only delete 3 search histories at a time. To delete more, specify 'all'.")
+            print(f">>>  {requestor.name}'s delete request was denied due to too large a size ({int(to_delete/2)} items/{to_delete} messages).")
+            
+        else:
+            count = 0
+            requested = to_delete
+            async for message in message.channel.history(limit=None):
+                count += 1
+                
+                if message.author == requestor and message.content.startswith('!delete'):
+                    count -= 1
+                    await message.delete()
+                    print(f"-->  Deleted  '{message.content}'  request message.")
+                elif message.author == client.user and "you can only" in message.content:
+                    count -= 1
+                    await message.delete()
+                    print(f"-->  Deleted a 'to_delete > 6' warning message.")
+                elif message.author == requestor and message.content.startswith('.'):
+                    await message.delete()
+                    print(f"-->  Deleted message #{count} of {requested} (query '{message.content}').")
+                    to_delete -= 1
+                    if to_delete == 0:  break
+                elif message.author == client.user:
+                    await message.delete()
+                    print(f"-->  Deleted message #{count} of {requested} (ngram plot).")
+                    to_delete -= 1
+                    if to_delete == 0:  break
+
+            print(f"\n>>>  Finished deletion request from {requestor.name} in {round(time.perf_counter()-start,3)} seconds.\n")
+    
+
+
+    #*******************************************#
+    #                                           #
+    #   Search history deletion handler (all)   #
+    #                                           #
+    #*******************************************#
+    if message.content.startswith('!delete') and 'all' in message.content:
+        requestor = message.author
+        print(f"\n>>>  {requestor.name} requested deletion of all messages.")
+
+        count = 0
+        async for message in message.channel.history(limit=None):
+            count += 1
+            if message.author == requestor and message.content.startswith('!delete'):
+                count -= 1
+                await message.delete()
+                print(f"-->  Deleted  '{message.content}'  request message.")
+            elif message.author == client.user and "you can only" in message.content:
+                count -= 1
+                await message.delete()
+                print(f"-->  Deleted a 'to_delete > 6' warning message.")
+            elif message.author == requestor and message.content.startswith('.'):
+                await message.delete()
+                print(f"-->  Deleted message #{count} (query '{message.content}').")
+            elif message.author == client.user:
+                await message.delete()
+                print(f"-->  Deleted message #{count} (ngram plot).")
+
+        print(f"\n>>>  Finished 'all' deletion request from {requestor.name} in {round(time.perf_counter()-start,3)} seconds.\n")
+
+
+
+
+client.run(TOKEN)
